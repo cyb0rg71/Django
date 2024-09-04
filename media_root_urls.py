35a1d6cd9c1244070564a65a3195623fb7b1f6a4
Salman Chowdhury Tasin <118939850+cyb0rg71@users.noreply.github.com>
2024-09-05 01:53:23 +0600

    Create media_root_urls.py

---
+++ media_root_urls.py
@@ -0,0 +1,10 @@
+# core/urls.py (or your main urls.py)
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    # Your other URL patterns here...
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
